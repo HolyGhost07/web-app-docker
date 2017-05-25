71ef9e0dd58b05fd65d27336c12556c2f4fce2d9
Eduard Kiss <e.kiss@office.ngs.ru>
2017-05-25 23:58:55 +0700

    Init repository

---
+++ code/www/app.php
@@ -0,0 +1,22 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write(
+        "hostname - {$_SERVER['HOSTNAME']}"
+    );
+
+    return $response;
+});
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->run();
